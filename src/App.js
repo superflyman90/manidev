277fc6a2677c27e6d89d3908a50e8c78cc252e12
superflyman90 <superflyman90@gmail.com>
2022-06-28 19:32:12 +0200

    add photo cv

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import User from './components/User';
 
 const App = () => {
     return (
-        <div className='app'>
-            <h1>ManiDev</h1>
+        <div className='App'>
+            <div className="grid__container">
+                <div className="sidebar">
+                    <User />
+                </div>
+                <div className="main">main</div> 
+            </div>
         </div>
     );
 };
---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div className='user'>
+            <img src="./images/avatar.jpg" alt="Manfred Parbatia" />
+            <h1 className="user__name">Manfred Parbatia</h1>
+            <p className="user__profession">Développeur Web</p>
+        </div>
+    );
+};
+
+export default User;
